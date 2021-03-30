3639d2737995526c448209e15748a218c565b166
Mr Green <dwgreen@hwdsb.on.ca>
2021-03-30 11:13:54 -0400

    rotate worked on, not done

---
+++ rotate.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+windowSize = (500, 500)
+screen = pygame.display.set_mode(windowSize)
+white = pygame.color.Color("#FFFFFF")
+
+robot1 = pygame.image.load('graphics/UglyBot 2000.png')
+robot1.set_colorkey((0, 0, 0))
+angle = 0
+rotatedImage = pygame.transform.rotate(robot1, angle)
+
+
+done = False
+
+while not done:
+    screen.fill(white)
+    screen.blit(rotatedImage, (0, 0))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_u]:
+        rotatedImage = pygame.transform.rotate(rotatedImage, 5)
+        screen.blit(rotatedImage, (0, 0))
+    if keys[pygame.K_o]:
+        rotatedImage = pygame.transform.rotate(rotatedImage, -5)
+        screen.blit(rotatedImage, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip()
+    clock.tick()
+
+pygame.quit()
