a95ce4ca054dc47d53ff4f723716d6a875e88289
Mr Green <dwgreen@hwdsb.on.ca>
2021-04-09 13:10:08 -0400

    fixing up

--- gameStuff.py
+++ gameStuff.py
@@ -1,21 +1,22 @@
 import pygame
 import robot
 import cards
+from src.config import MAX_X, MAX_Y, GRID_COLS, GRID_ROWS
 
 pygame.init()
 
 
-maxX = 1000
-maxY = 1000
+MAX_X = 1000
+MAX_Y = 1000
 
-gridRows = 11
-gridCols = 11
-gridXsize = maxX/gridCols
-gridYzize = maxY/gridRows   # ******** not int
-windowSize = [maxX, maxY]
+GRID_ROWS = 11
+GRID_COLS = 11
+gridXsize = MAX_X/GRID_COLS
+gridYzize = MAX_Y/GRID_ROWS   # ******** not int
+windowSize = [MAX_X, MAX_Y]
 screen = pygame.display.set_mode(windowSize)
 gameBoard = pygame.image.load('graphics/2016doodle.jpg')
-gameBoard = pygame.transform.scale(gameBoard, (1000, 1000))
+gameBoard = pygame.transform.scale(gameBoard, (MAX_X, MAX_y))
 robot1 = robot.Robot('Lloyd', 0, 0)
 robot1.setMoveMult(gridXsize)  # int is not callable
 robot1.img = pygame.image.load('graphics/UR1.png')
@@ -47,7 +48,7 @@ while not done:
 
     if keys[pygame.K_k] and robot1.dir == 2 and not keyPressed:
         keyPressed = True
-        if robot1.y < maxY - 40:
+        if robot1.y < MAX_Y - 40:
             robot1.moveDown()
             screen.blit(gameBoard, (0, 0))
             screen.blit(robot1.img, (robot1.x, robot1.y))
@@ -55,7 +56,7 @@ while not done:
 
     if keys[pygame.K_l] and robot1.dir == 1 and not keyPressed:
         keyPressed = True
-        if robot1.x < maxX - 40:
+        if robot1.x < MAX_X - 40:
             robot1.moveRight()
             screen.blit(gameBoard, (0, 0))
             screen.blit(robot1.img, (robot1.x, robot1.y))
